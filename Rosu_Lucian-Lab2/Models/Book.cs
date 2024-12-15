65069e9c2286750faddd55206dc6c9e03280f13e
LUCIAN-DAVID ROȘU <lucian.rosu@stud.ubbcluj.ro>
2024-12-16 00:04:18 +0200

    Add project files.

---
+++ Rosu_Lucian-Lab2/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace Rosu_Lucian_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+    } //navigation property
+
+}
+
